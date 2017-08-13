9762bc1f286251e472844978557ecf7ebef419b0
ahmetkapusuz@gmail.com <ahmetkapusuz@gmail.com>
2017-08-13 15:00:36 +0300

    first commit

---
+++ src/main/java/com/javabunga/springbootexample/SpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.javabunga.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+}
